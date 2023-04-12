6243fe991d70e5a691a8aae44b8c570c46a42a98
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-04-12 12:57:30 -0400

    some more initial changes in setup

---
+++ new_scripts/gic_gicore_tool.py
@@ -0,0 +1,5 @@
+"""
+The goal of this script is to convert the original GIC_GICore_Tool.py script in ~/original_scripts
+to open source solutions such as GRASS, PyQGIS, RasterIO, etc.
+Hopefully ChatGPT will be heavily leveraged to help make this conversion process smoother
+"""
