a03f997db8131a1f27f55d3a8aba5f0a0cbac6eb
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-08-01 11:11:55 -0400

    pushing up in effort to clean up local mac

--- new_scripts/gic_gicore_tool.py
+++ new_scripts/gic_gicore_tool.py
@@ -1,5 +1,52 @@
-"""
-The goal of this script is to convert the original GIC_GICore_Tool.py script in ~/original_scripts
-to open source solutions such as GRASS, PyQGIS, RasterIO, etc.
-Hopefully ChatGPT will be heavily leveraged to help make this conversion process smoother
-"""
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterFeatureSource,
+    QgsProcessingParameterString
+)
+
+class ExampleAlgorithm(QgsProcessingAlgorithm):
+
+    INPUT_LAYER = 'INPUT_LAYER'
+    TEXT_PARAMETER = 'TEXT_PARAMETER'
+
+    def initAlgorithm(self, config=None):
+        # Define input parameters
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT_LAYER,
+                'Input vector layer',
+                [QgsProcessing.TypeVector]
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterString(
+                self.TEXT_PARAMETER,
+                'Text parameter'
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        # Retrieve parameter values
+        input_layer = self.parameterAsSource(parameters, self.INPUT_LAYER, context)
+        text_parameter = self.parameterAsString(parameters, self.TEXT_PARAMETER, context)
+
+        # Perform processing tasks using input_layer and text_parameter
+        # ...
+
+        return {}  # Return an empty dictionary as this is a minimal example
+
+    def name(self):
+        return 'example_algorithm'
+
+    def displayName(self):
+        return 'Example Algorithm'
+
+    def group(self):
+        return 'Examples'
+
+    def groupId(self):
+        return 'examples'
+
+    def createInstance(self):
+        return ExampleAlgorithm()
